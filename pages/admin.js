4ab58b1f890af703ad1d2750462aa985fbc9be40
shaikhmuskan1024@gmail.com <shaikhmuskan1024@gmail.com>
2023-07-13 22:44:29 +0530

    commit-2

--- pages/admin.js
+++ pages/admin.js
@@ -1,4 +1,4 @@
-import AdminPanel from '@/components/AdminPanel'
+// import AdminPanel from '@/components/AdminPanel'
 import Navbar from '@/components/Navbar'
 import React from 'react'
 
@@ -6,7 +6,7 @@ export default function admin() {
   return (
     <div>
         <Navbar />
-        <AdminPanel />
+        {/* <AdminPanel /> */}
     </div>
   )
 }
