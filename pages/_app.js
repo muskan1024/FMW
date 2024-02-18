37c1e47198b0b9f8def42df41873304e44904147
muskan1024 <shaikhmuskan1024@gmail.com>
2024-02-18 18:21:00 +0530

    .

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
@@ -7,7 +7,7 @@ export default function App({ Component, pageProps }) {
     <>
       <Component {...pageProps} />
       <Analytics />
-      <SpeedInsights/>
+      <SpeedInsights />
     </>
   );
 }
